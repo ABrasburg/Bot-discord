34bbff8f2f3dfd019db6c79d529e19c2be87c690
ABrasburg <abrasburg@fi.uba.ar>
2025-05-06 11:39:29 -0300

    Agrego guild

--- bot.py
+++ bot.py
@@ -8,11 +8,21 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 TOKEN = TOKEN.lstrip('{')
 TOKEN = TOKEN.rstrip('}')
+GUILD = os.getenv('DISCORD_GUILD')
+GUILD = GUILD.lstrip('{')
+GUILD = GUILD.rstrip('}')
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 client.run(TOKEN)
